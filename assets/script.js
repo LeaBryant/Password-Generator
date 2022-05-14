35e13030cefe9298a1fb5201f71565867c31ea80
LeaBryant <ldbryant3220@gmail.com>
2022-05-14 10:24:59 -0700

    added updated js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chars = "abcdefghijklmnopqrstuvwxyz".split("");
+var randNum = "1234567890".split("");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var password = Math.floor(Math.random() * chars);
+
 
   passwordText.value = password;
 
@@ -12,3 +17,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+document.getElementById ("#generate").value;
+function generatePassword() {
+    var generatedPassword = "generated password" 
+    console.log("clicked generate password");
+    generatedPassword = "";
+    return generatedPassword;
+}
+
+
+var lengthPass = prompt("please enter password length")
+
+//   var lowerChar = confirm ("do you want lower case characters")
+
+// //Added object code 
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   var randNum = Math.floor(Math.random() * chars);
+
+
+//   passwordText.value = password;
+// }
+
+// document.getElementById ("#generate").value =
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// // Create a alert box //
+
