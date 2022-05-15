506db40c3ddfe0b33b86adc6a4ff33c6065e8142
LeaBryant <ldbryant3220@gmail.com>
2022-05-14 22:55:26 -0700

    I forsure need mre time on this

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var chars = "abcdefghijklmnopqrstuvwxyz".split("");
-var randNum = "1234567890".split("");
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var symbols = "!@#$%^&*(_-)(";
+var length = char[8];
+
+// Create a alert box //
+function alert(){
+  prompt("How many characters do you want your password to have?");
+    confirm ("Click okay to confirm including special characters");
+    confirm("Click okay to confirm including numarical characters");
+()};
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  var password = Math.floor(Math.random() * chars);
-
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-document.getElementById ("#generate").value;
 function generatePassword() {
-    var generatedPassword = "generated password" 
+  var password = "generatePassword";
     console.log("clicked generate password");
-    generatedPassword = "";
-    return generatedPassword;
+
+    return generatePassword;
 }
+console.log(generatePassword() );
+
+var randNum = randNum;
+
+function writePassword(){
+    var password = writePassword();
+    var passwordText = document.querySelector("#password");
+    var randNum = Math.floor(Math.random() * chars.length);
+    
+    passwordText.vaule = password;
+    
+  }
+    
+document.getElementById ("#generate").value =
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);{
+  generateBtn.innerText = generatePassword(hasUpper, hasLower, hasNumbers, hasSymbols, length);
+  console.log("generatebut");
+
+}
+generatedPassword = "";
 
 
-var lengthPass = prompt("please enter password length")
 
-//   var lowerChar = confirm ("do you want lower case characters")
-
-// //Added object code 
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   var randNum = Math.floor(Math.random() * chars);
-
-
-//   passwordText.value = password;
-// }
-
-// document.getElementById ("#generate").value =
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// // Create a alert box //
 
